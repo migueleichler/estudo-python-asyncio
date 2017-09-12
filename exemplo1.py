03052607a7054e604c0f522447de3e179d1b28b4
miguel <migueleichler23@gmail.com>
2017-09-12 16:33:00 -0300

    Adição da função de cálculo do tempo total do Loop de eventos.

--- exemplo1.py
+++ exemplo1.py
@@ -1,12 +1,15 @@
 import asyncio
+import time
 
+def get_intervalo_tempo(inicio):
+    return int(time.time() - inicio)
 
 async def incrementar_por_n_segundos(loop, segundos):
     num = 0
-    end_time = loop.time() + segundos
+    fim_loop = loop.time() + segundos
     while True:
         num += 1
-        if (loop.time() + 1.0) >= end_time:
+        if (loop.time() + 1.0) >= fim_loop:
             break
         await asyncio.sleep(1)
 
@@ -17,5 +20,7 @@ tasks = [
     loop.create_task(incrementar_por_n_segundos(loop, 3.0)),
     loop.create_task(incrementar_por_n_segundos(loop, 5.0)),
 ]
+inicio_loop = time.time()
 loop.run_until_complete(asyncio.wait(tasks))
+print(get_intervalo_tempo(inicio_loop))
 loop.close()
