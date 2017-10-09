e7841a3f25b41db2a9f99ecc86647cbb35b48984
migueleichler <migueleichler23@gmail.com>
2017-10-09 02:28:19 -0300

    Adição de comentários de definição das palavras-chave async e await no código-fonte do programa exemplo01.py

--- exemplo01/exemplo01.py
+++ exemplo01/exemplo01.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 from datetime import datetime
 
+
 nomes_tasks = ['A', 'B', 'C', 'D', 'E', 'F']
 
 def get_intervalo_tempo(inicio):
@@ -9,6 +10,8 @@ def get_intervalo_tempo(inicio):
     intervalo = (atual - inicio).total_seconds()
     return intervalo
 
+# 1 - Para indicar que a subrotina é assíncrona é preciso adicionar a
+# palavra-chave async antes da palavra-chave def.
 async def incrementar_por_n_segundos(segundos):
     num = 0
     inicio = datetime.now()
@@ -16,8 +19,10 @@ async def incrementar_por_n_segundos(segundos):
     while segundos >= num:
         num += 1
         print('Iteração %i da Task %s - %i segundos' %
-          (num, nome_task, get_intervalo_tempo(inicio)))
-        # O método sleep recebe como parâmetro a quantidade de segundos que
+        # 2 - A palavra-chave await é parte da biblioteca asyncio e ela define
+        # que a execução da subrotina incrementar_por_n_segundos será suspensa
+        # até o término do método sleep.
+        # 3 - O método sleep recebe como parâmetro a quantidade de segundos que
         # a task ficará em estado de pausa.
         await asyncio.sleep(1)
 
