3fd9a6cb59276a716ee12dc703bc0cc312d66071
migueleichler <migueleichler23@gmail.com>
2017-10-15 12:45:46 -0200

    Novas melhorias no código do exemplo 01.

--- exemplo01/exemplo01.py
+++ exemplo01/exemplo01.py
@@ -1,24 +1,26 @@
 import asyncio
-import time
 from datetime import datetime
 
 
 nomes_tasks = ['A', 'B', 'C', 'D', 'E', 'F']
+
 
 def get_intervalo_tempo(inicio):
     atual = datetime.now()
     intervalo = (atual - inicio).total_seconds()
     return intervalo
 
+
 # 1 - Para indicar que a subrotina é assíncrona é preciso adicionar a
 # palavra-chave async antes da palavra-chave def.
-async def incrementar_por_n_segundos(segundos):
+async def iterar_por_n_segundos(segundos):
     num = 0
     inicio = datetime.now()
     nome_task = nomes_tasks.pop(0)
     while segundos >= num:
         num += 1
-        print('Iteração %i da Task %s - %i segundos' %
+        args = (num, nome_task, get_intervalo_tempo(inicio))
+        print('Iteração %i da Task %s - %i segundos desde o inicio do loop de eventos' % args)
         # 2 - A palavra-chave await é parte da biblioteca asyncio e ela define
         # que a execução da subrotina incrementar_por_n_segundos será suspensa
         # até o término do método sleep.
@@ -29,8 +31,8 @@ async def incrementar_por_n_segundos(segundos):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     tasks = [
-        loop.create_task(incrementar_por_n_segundos(3)),
-        loop.create_task(incrementar_por_n_segundos(5)),
+        loop.create_task(iterar_por_n_segundos(3)),
+        loop.create_task(iterar_por_n_segundos(5)),
     ]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
