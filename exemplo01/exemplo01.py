bbaa69f5b1d75f12d63f753b9c2ce2b81631be18
migueleichler <migueleichler23@gmail.com>
2017-10-26 01:08:03 -0200

    Melhorias no código dos exemplos 01 e 02.

--- exemplo01/exemplo01.py
+++ exemplo01/exemplo01.py
@@ -3,23 +3,23 @@ from datetime import datetime
 
 
 nomes_tasks = ['A', 'B', 'C', 'D', 'E', 'F']
-MSG_INI = 'Início da Iteração %i da Task %s - %s'
-MSG_FIM = 'Fim da Iteração %i da Task %s - %s'
+MSG_INI = 'Início da Iteração {} da Task {} - {}'
+MSG_FIM = 'Fim da Iteração {} da Task {} - {}'
 
 
-def get_intervalo_tempo(inicio):
+def print_duracao_task(task, inicio):
     atual = datetime.now()
-    intervalo = (atual - inicio).total_seconds()
-    return intervalo
+    intervalo = int((atual - inicio).total_seconds())
+    print('\nDuração total Task {}: {} segundos\n'.format(task, intervalo))
 
 
 # 1 - Para indicar que a subrotina é assíncrona é preciso adicionar a
 # palavra-chave async antes da palavra-chave def.
-async def iterar_por_n_segundos(segundos):
+async def iterar_por_n_segundos(segundos, loop):
     inicio_task = datetime.now()
     task = nomes_tasks.pop(0)
     for i in range(1, segundos + 1):
-        print(MSG_INI % (i, task, datetime.now().strftime('%H:%M:%S')))
+        print(MSG_INI.format(i, task, datetime.now().strftime('%H:%M:%S')))
         # 2 - A palavra-chave await é parte da biblioteca asyncio e ela define
         # que a execução da subrotina incrementar_por_n_segundos será suspensa
         # até o término do método sleep.
@@ -27,16 +27,15 @@ async def iterar_por_n_segundos(segundos):
         # a task ficará em estado de pausa.
         await asyncio.sleep(1)
 
-        print(MSG_FIM % (i, task, datetime.now().strftime('%H:%M:%S')))
+        print(MSG_FIM.format(i, task, datetime.now().strftime('%H:%M:%S')))
 
-    duracao_task = get_intervalo_tempo(inicio_task)
-    print('\nDuração total Task %s: %i segundos\n' % (task, duracao_task))
+    print_duracao_task(task, inicio_task)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     tasks = [
-        loop.create_task(iterar_por_n_segundos(3)),
-        loop.create_task(iterar_por_n_segundos(5)),
+        loop.create_task(iterar_por_n_segundos(3, loop)),
+        loop.create_task(iterar_por_n_segundos(5, loop)),
     ]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
--- exemplo02/exemplo02.py
+++ exemplo02/exemplo02.py
@@ -1,10 +1,22 @@
 import asyncio
 
 
-def exibir_alfabeto(letras, loop):
-    print(letras.pop(0))
-    if len(letras) > 0:
-        loop.call_later(1, exibir_alfabeto, letras, loop)
+# O loop de eventos pode agendar a chamada de funções com base no valor
+# do temporizador mantido no loop.
+#
+# Há dois métodos que podem ser utilizados nesse "agendamento":
+# 1 - call_soon() --> para casos em que o valor do temporizador do loop de
+#                     eventos não é importante.
+# 2 - call_later() --> para casos em que se deseja definir o intervalo de tempo
+#                      que se deseja postergar a execução da função.
+# Callback é uma função que é passada como argumento para outra função
+# e é invocada após algum tipo de eventoo término da execução da sua
+# função pai.
+def exibir_palavra(palavra, loop):
+    print(palavra[0])
+    palavra = palavra[1:]
+    if len(palavra) > 0:
+        loop.call_later(1, exibir_palavra, palavra, loop)
     else:
         loop.stop()
 
@@ -12,8 +24,7 @@ def exibir_alfabeto(letras, loop):
 loop = asyncio.get_event_loop()
 
 # Chamada incial da função exibir_alfabeto()
-letras = list('abcdefghijklmnopqrstuvwxyz')
-loop.call_soon(exibir_alfabeto, letras, loop)
+loop.call_soon(exibir_palavra, 'paralelepipedo', loop)
 
 loop.run_forever()
 loop.close()
